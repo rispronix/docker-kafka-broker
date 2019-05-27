473cbf865424a436d292342f8f5ac076ddbdeac8
Richard Rowley <richard.rowley@live.com>
2019-05-27 13:45:28 +0100

    added kafka topic

--- start.sh
+++ start.sh
@@ -2,3 +2,6 @@
  
 $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic iris
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic titanic
